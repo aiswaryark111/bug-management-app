bc09bc854727fda9896eed5d99f55aa3068ac15f
Basil Mathew <basil.mathew@sreegcloudgmail.onmicrosoft.com>
2022-05-17 16:18:08 +0530

    Screens for navigation

---
+++ BugsApp/src/screens/createBugs/createBugs.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Text, View } from "react-native"
+import styles from "../../themes/styles"
+
+const CreateBug = () =>{
+    return(
+        <View style={styles.container}>
+            <Text>
+                Create Bugs Here
+            </Text>
+        </View>
+    )
+}
+
+export default CreateBug
---
+++ BugsApp/src/screens/unresolvedBugs/unresolvedBugs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../../themes/styles"
+
+const UnresolvedBugs = () =>{
+    return (
+        <View style={styles.container}>
+            <Text>
+                Unresolved Bugs!
+            </Text>
+        </View>
+    )
+}
+
+export default UnresolvedBugs
